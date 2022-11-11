4bf92310529448a06985648ffd2391bcfe4d91b5
Mounika_tech <92107249+mounikamuddala@users.noreply.github.com>
2022-11-11 12:57:50 +0530

    Update Gradeapplication.java

--- Gradeapplication.java
+++ Gradeapplication.java
@@ -9,16 +9,16 @@ public class Gradeapplication {
 		System.out.println("please enter your marks");
 		Scanner sc = new Scanner(System.in);
 		int marks = sc.nextInt();
-		if(marks >= 80  &&   marks>=100 ) {
+		if(marks >= 80  &&   marks<=100 ) {
 			System.out.println("your grade is a");
 		}
-		else if(marks >=   60 &&   marks>= 800 ) {
+		else if(marks >=   60 &&   marks<= 800 ) {
 			System.out.println("your grade is b");
 		}
-		else if(marks >= 40  &&   marks>=60 ) {
+		else if(marks >= 40  &&   marks<=60 ) {
 			System.out.println("your grade is c");
 		}
-		else if(marks >= 30  &&   marks>= 40 ) {
+		else if(marks >= 30  &&   marks<= 40 ) {
 			System.out.println("your grade is d");
 		}
 		else {
